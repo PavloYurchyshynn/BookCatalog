2e7f6054f8dbfea035285425dbfbd1586c8a9882
PavloYurchyshynn <gamepaha123@gmail.com>
2023-12-13 14:44:19 +0200

    feat: entity framework context

---
+++ BookCatalog/DependencyInjection/DIConfig.cs
@@ -0,0 +1,15 @@
+﻿using BookCatalog.DataAccess.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookCatalog.API.DependencyInjection
+{
+    public static class DIConfig
+    {
+        public static void AddDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            string? connection = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<BookCatalogContext>(options => options.UseSqlServer(connection));
+        }
+    }
+}
--- BookCatalog/Program.cs
+++ BookCatalog/Program.cs
@@ -1,7 +1,10 @@
+using BookCatalog.API.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDatabase(builder.Configuration);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
