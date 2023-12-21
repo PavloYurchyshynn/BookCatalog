b152b826201242b86e55dbe983d95bec358fd10d
PavloYurchyshynn <gamepaha123@gmail.com>
2023-12-21 16:29:05 +0200

    feat: unit tests

---
+++ BookCatalog.Application.UnitTests/Services/BookServiceTests.cs
@@ -0,0 +1,83 @@
+using AutoMapper;
+using BookCatalog.Application.MappingProfiles;
+using BookCatalog.Application.Models.Book;
+using BookCatalog.Application.Services;
+using BookCatalog.Application.Services.Contracts;
+using BookCatalog.Core.Entities;
+using BookCatalog.DataAccess.DapperRepositories.Contracts;
+using BookCatalog.DataAccess.Repositories.Contracts;
+using Moq;
+using System.Linq.Expressions;
+
+namespace BookCatalog.Application.UnitTests.Services
+{
+    public class BookServiceTests
+    {
+        private readonly Mock<IBookRepository> _bookRepository;
+        private readonly Mock<IDapperBookRepository> _dapperBookRepository;
+        private readonly IBookService _bookService;
+        private readonly IMapper _mapper;
+
+        public BookServiceTests()
+        {
+            _mapper = new MapperConfiguration(config =>
+            {
+                config.AddMaps(typeof(BookProfile));
+            }).CreateMapper();
+            _bookRepository = new Mock<IBookRepository>();
+            _dapperBookRepository = new Mock<IDapperBookRepository>();
+            _bookService = new BookService(_mapper, _bookRepository.Object, _dapperBookRepository.Object);
+        }
+
+        [Fact]
+        public void GetAllBooks_Should_Return_Not_Empty_List()
+        {
+            // Arrage
+            List<Book> books = new List<Book>()
+            {
+                new Book() { Author = "Pasha", Description = "Good book!", Name = "Harry Potter", Price = 500 },
+                new Book() { Author = "Pasha1", Description = "Good book!1", Name = "Harry Potter1", Price = 600 },
+                new Book() { Author = "Pasha2", Description = "Good book!2", Name = "Harry Potter2", Price = 700 }
+            };
+            _bookRepository.Setup(b => b.GetAll()).Returns(books.AsQueryable());
+
+            // Act
+            var resultBooks = _bookService.GetAllBooks();
+
+            // Assert
+            Assert.NotEmpty(resultBooks);
+        }
+
+        [Fact]
+        public async Task AddBook_Result_Should_Be_Equal_To_Model()
+        {
+            // Arrage
+            var bookModel = new AddBookModel() { Author = "Pasha", Description = "test", Name = "Book about love", Price = 200 };
+            var bookEntity = _mapper.Map<Book>(bookModel);
+
+            _bookRepository.Setup(b => b.AddAsync(bookEntity)).Returns(Task.FromResult(bookEntity));
+
+            // Act
+            var result = await _bookService.AddBookAsync(bookModel);
+
+            // Accert
+            Assert.Equal(bookModel.Name, result.Name);
+        }
+
+        [Fact]
+        public async Task DeleteBook_Should_Return_Success_Result()
+        {
+            // Arrage
+            Guid bookId = Guid.NewGuid();
+            var book = new Book() { id = bookId, Author = "Pasha", Description = "test", Name = "Book about love", Price = 200 };
+
+            _bookRepository.Setup(b => b.GetFirstAsync(It.IsAny<Expression<Func<Book, bool>>>())).Returns(Task.FromResult(book));
+
+            // Act
+            var result = await _bookService.DeleteBookAsync(bookId);
+
+            // Assert
+            Assert.Contains(result.status, "Success");
+        }
+    }
+}
---
+++ BookCatalog.Application.UnitTests/Services/OrderServiceTests.cs
@@ -0,0 +1,95 @@
+﻿using AutoMapper;
+using BookCatalog.Application.MappingProfiles;
+using BookCatalog.Application.Models.Order;
+using BookCatalog.Application.Services;
+using BookCatalog.Application.Services.Contracts;
+using BookCatalog.Core.Entities;
+using BookCatalog.DataAccess.Repositories.Contracts;
+using Moq;
+using System.Linq.Expressions;
+
+namespace BookCatalog.Application.UnitTests.Services
+{
+    public class OrderServiceTests
+    {
+        private readonly Mock<IOrderRepository> _orderRepository;
+        private readonly Mock<IOrderItemRepository> _orderItemRepository;
+        private readonly Mock<IBasketRepository> _basketRepository;
+        private readonly Mock<IBasketItemRepository> _basketItemRepository;
+        private readonly IOrderService _orderService;
+        private readonly IMapper _mapper;
+
+        public OrderServiceTests()
+        {
+            _orderRepository = new Mock<IOrderRepository>();
+            _orderItemRepository = new Mock<IOrderItemRepository>();
+            _basketRepository = new Mock<IBasketRepository>();
+            _basketItemRepository = new Mock<IBasketItemRepository>();
+            _mapper = new MapperConfiguration(config =>
+            {
+                config.AddMaps(typeof(OrderProfile));
+            }).CreateMapper();
+            _orderService = new OrderService(_orderRepository.Object, _orderItemRepository.Object, _basketRepository.Object, _basketItemRepository.Object, _mapper);
+        }
+
+        [Fact]
+        public async Task MakeOrder_Should_Return_Order()
+        {
+            // Arrage
+
+            Guid basketId = Guid.NewGuid();
+            var orderModel = new MakeOrderModel() { BasketId = basketId, DeliveryAddress = "Home", Email = "test@x.com", Name = "Pasha", PhoneNumber = 23432432 };
+            var basketItems = new List<BasketItem>()
+            {
+                new BasketItem() { BookId = Guid.NewGuid(), Quantity = 5, BasketId = basketId },
+                new BasketItem() { BookId = Guid.NewGuid(), Quantity = 5, BasketId = basketId },
+                new BasketItem() { BookId = Guid.NewGuid(), Quantity = 5, BasketId = basketId },
+            };
+            var basket = new Basket() { id = basketId, TotalQuantity = 15, TotalAmount = 1500 };
+            var order = new Order()
+            {
+                BasketId = basketId,
+                DeliveryAddress = orderModel.DeliveryAddress,
+                Email = orderModel.Email,
+                Name = orderModel.Name,
+                PhoneNumber = orderModel.PhoneNumber,
+                Price = basket.TotalAmount
+            };
+            var addedOrder = new Order()
+            {
+                id = Guid.NewGuid(),
+                BasketId = order.BasketId,
+                DeliveryAddress = order.DeliveryAddress,
+                Email = order.Email,
+                Name = order.Name,
+                PhoneNumber = order.PhoneNumber,
+                Price = order.Price
+            };
+            var query = basketItems.Where(x => x.BasketId == basketId).AsQueryable();
+
+            _basketItemRepository.Setup(x => x.GetWhere(It.IsAny<Expression<Func<BasketItem, bool>>>())).Returns(query);
+            _basketRepository.Setup(x => x.GetFirstAsync(It.IsAny<Expression<Func<Basket, bool>>>())).Returns(Task.FromResult(basket));
+            _orderRepository.Setup(x => x.AddAsync(order)).Returns(Task.FromResult(addedOrder));
+            foreach (var item in basketItems)
+            {
+                var orderItem = new OrderItem()
+                {
+                    BookId = item.BookId,
+                    OrderId = addedOrder.id,
+                    Quantity = item.Quantity,
+                };
+
+                _orderItemRepository.Setup(x => x.AddAsync(orderItem));
+                _basketItemRepository.Setup(x => x.DeleteAsync(item));
+            }
+            _basketRepository.Setup(x => x.UpdateAsync(basket));
+
+            // Act
+            var result = await _orderService.MakeOrderAsync(orderModel);
+
+            // Assert
+            Assert.NotNull(result);
+
+        }
+    }
+}
